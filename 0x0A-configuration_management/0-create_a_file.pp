bb3bbd9a746ce73aac921be59aaea6bf68c95941
Nwaneri Georginia <106970884+Georgy22@users.noreply.github.com>
2023-01-11 20:58:45 +0100

    I love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-I love Puppet
+# create file named school in tmp with below specification
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
